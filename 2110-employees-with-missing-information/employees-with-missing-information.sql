5004d5fac596fcc63febcc03ddc606f0286c9285
Potharaju Ushasree <pushasree8888@gmail.com>
2026-08-14 14:27:40 +0530

    Time: 760 ms (15.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select e.employee_id 
+from Employees as e left join Salaries as s 
+on e.employee_id = s.employee_id 
+where s.salary is null 
+union 
+select s.employee_id 
+from Employees as e right join Salaries as s
+on s.employee_id = e.employee_id
+where e.name is null
+order by employee_id;
